1b4019d9841c40fc998eac9cf203ce4f2e2b204e
diego rivas <diego.rivas@ucsp.edu.pe>
2017-11-19 00:23:04 -0500

    query

--- java/repository/AlumnoRepository.java
+++ java/repository/AlumnoRepository.java
@@ -1,9 +1,15 @@
 package repository;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import domain.Alumno;
 
 public interface AlumnoRepository extends CrudRepository<Alumno, String>{
   List<Alumno> findAll();
+
+  @Query("select a from Alumno a where a.nombres = ?1")
+  Alumno findAlumnoName(String name);
+
 }
